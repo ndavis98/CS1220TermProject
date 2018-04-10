8dc26886834adbb81b900061319cd672a7c3727f
amcneil1998 <alexanderjmcneil@cedarville.edu>
2018-04-10 16:56:02 -0400

    Added header files for wire and gate classes

--- TermProject/TermProject/GateClass.h
+++ TermProject/TermProject/GateClass.h
@@ -1,1 +1,32 @@
-#pragma once
+// Author:  Alex McNeil
+// Date:    4/10/2018
+// Purpose: Header file for Gate class
+#include <string>
+#include "WireClass.h"
+#ifndef Gate_H
+#define Gate_H
+using namespace std;
+
+class Gate {
+	public:
+		Gate();//default constructor
+		Gate(string name, int Delay, Wire In1, Wire In2, Wire outPut); //specified constructor
+		string setName(const string name);
+		int  setDelay(const int delay);
+		char setIn1(const char in1);
+		char setIn2(const char in1);
+		char setOutPut(const char outPut);
+
+
+	protected:
+
+	private:
+		string type;
+		int delay;
+		Wire in1;
+		Wire in2;
+		Wire outPut;
+
+
+};
+#endif //Gate_H
--- TermProject/TermProject/WireClass.h
+++ TermProject/TermProject/WireClass.h
@@ -1,1 +1,28 @@
-#pragma once
+// Author:  Alex McNeil
+// Date:    4/10/2018
+// Purpose: Header file for Wire class
+#include <string>
+#include "GateClass.h"
+#ifndef WIRE_H
+#define WIRE_H
+using namespace std;
+
+class Wire{
+	public:
+		Wire();
+		Wire(int value, string name, int index, Gate *history, vector <Gate *>);
+		int setValue(const int &value);
+		string setName(const string &name);
+		int setIndex(const int &index);
+		Gate setHistory(const Gate &history);
+		vector setOutPut(const Gate &Gate)
+	protected:
+
+	private:
+		int value;
+		string name;
+		int index;
+		Gate* history; //this is the input to the wire
+		vector <Gate *> out;
+};
+#endif //WIRE_H
